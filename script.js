dc11e7681c9b43c1006cf25d1a936a0de5cd2030
toothless13 <50870607+toothless13@users.noreply.github.com>
2023-01-18 15:11:10 +0000

    Add responsive nav bar to pages using JavaScript

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const dropNavBtn = document.getElementById("btn");
 const dropMenu = document.getElementById("desktop-nav");
+const header = document.querySelector("header");
+const main = document.querySelector("main");
 
 const toggleDropdown = function() {
-    dropMenu.classList.toggle("show");
+    dropMenu.classList.toggle("active");
+    header.classList.toggle("active");
+    main.classList.toggle("active");
+    dropNavBtn.classList.toggle("active");
+    console.log("toggleDropdown fired");
 };
 
-function test() {
-    console.log("Button pressed");
+function dropNav (e) {
+    // console.log("Button pressed");
+    // console.log(dropNavBtn);
+    // console.log(dropMenu);
+    e.stopPropagation();
+    toggleDropdown();
 }
 
-dropNavBtn.addEventListener("click", test);
+dropNavBtn.addEventListener("click", dropNav);
+
+document.documentElement.addEventListener("click", () => {
+    if (dropMenu.classList.contains("active")) {
+        toggleDropdown();
+    }
+});
+
+window.addEventListener("resize", function() {
+    // update your variables
+    const headerHeight = +header.offsetHeight;
+    dropMenu.style.top = headerHeight + "px";
+    main.style.top = headerHeight + "px";
+    console.log(dropMenu.style.top);
+  });
+  
+// const headerHeight = +header.offsetHeight;
+// dropMenu.style.top = headerHeight + "px";
+// main.style.top = headerHeight + "px";
+// console.log(dropMenu.style.top);
