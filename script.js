b81ada0868e0f85f39425890a77320d85d1d9dfd
toothless13 <50870607+toothless13@users.noreply.github.com>
2023-01-19 13:00:49 +0000

    Add initial carousel functionality

--- script.js
+++ script.js
@@ -38,8 +38,33 @@ window.addEventListener("resize", function() {
     main.style.top = headerHeight + "px";
     // console.log(main.style.top);
   });
-  
-// const headerHeight = +header.offsetHeight;
-// dropMenu.style.top = headerHeight + "px";
-// main.style.top = headerHeight + "px";
-// console.log(dropMenu.style.top);
+
+document.querySelectorAll(".carousel").forEach(carousel => {
+    const items = carousel.querySelectorAll(".carousel-item");
+    const buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel-button"></span>`;
+    });
+
+    carousel.insertAdjacentHTML("beforeend", `
+        <div class="carousel-nav">
+            ${buttonsHtml.join("")}
+        </div>
+    `);
+
+    const buttons = carousel.querySelectorAll(".carousel-button");
+
+    buttons.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            //Un-select all the items
+            items.forEach(item => item.classList.remove("carousel-item-selected"));
+            buttons.forEach(button => button.classList.remove("carousel-button-selected"));
+
+            //Select the correct item based on index
+            items[index].classList.add("carousel-item-selected");
+            button.classList.add("carousel-button-selected");
+        });
+    });
+    // Pre-select the first element of the items array and first button
+    items[0].classList.add("carousel-item-selected");
+    buttons[0].classList.add("carousel-button-selected");
+});
